ab88d9aed2e00546a61ca31e32c65965c446b1fa
trilu2000 <trilu@gmx.de>
2026-01-13 09:28:46 +0100

    bresser_weather.h

--- components/bresser_weather/bresser_weather.h
+++ components/bresser_weather/bresser_weather.h
@@ -8,9 +8,9 @@
 #include "WeatherSensor.h"
 
 // Pin definitions for D1 Mini with CC1101
-#define PIN_RECEIVER_CS 15           // D8
-#define PIN_RECEIVER_IRQ 4           // D2 (GD0)
-#define PIN_RECEIVER_GPIO 5          // D1 (GD2)
+#define PIN_RECEIVER_CS 10           // D8
+#define PIN_RECEIVER_IRQ 16          // D2 (GD0)
+#define PIN_RECEIVER_GPIO 4          // D1 (GD2)
 #define PIN_RECEIVER_RST RADIOLIB_NC // Not connected
 #define USE_CC1101
 
